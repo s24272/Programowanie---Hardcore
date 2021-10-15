793833efa39fd3d434a5656ef3383bb8c576fc19
s24272 <91730537+s24272@users.noreply.github.com>
2021-10-15 14:01:53 +0200

    03-25slajd

---
+++ 2zad.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int losnr, nr;
+    srand(time(NULL));
+    losnr = rand() % 100 + 1;
+    do{
+        std::cout<<"guess: ";
+        std::cin>>nr;
+        std::cout<<"\n";
+        if(nr==losnr)
+        {
+            std::cout<<"Brawo! Zgadles \n";
+        }
+        else if(losnr>nr)
+        {
+            std::cout<<"Za malo\n";
+        }
+        else if(losnr<nr)
+        {
+            std::cout<<"Za duzo\n";
+        }
+        
+    }while(nr!=losnr);
+}
+
